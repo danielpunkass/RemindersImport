3396eb4a0cbf2f1ecbb22dd09b3334146e3daae8
Daniel Jalkut <jalkut@red-sweater.com>
2013-02-01 14:47:05 -0500

    First version of RemindersImport.

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ RemindersImport
 ===============
 
 Command-line tool for migrating Mac OS X Reminders from the canonical system storage to The Omni Group's OmniFocus app.
+
+Description
+===========
+
+The Omni Group's <a href="http://www.omnigroup.com/products/omnifocus-iphone/">OmniFocus for iOS</a> takes advantage of a clever trick for allowing users to add items by voice instruction to Siri. OmniFocus will import items from the phone's central Reminders list, so that when you want to add something "to OmniFocus" you can just ask Siri to "add it to my reminders list."
+
+Unfortunately, <a href="http://www.omnigroup.com/products/omnifocus/">OmniFocus for Mac</a> doesn't support this. I love OmniFocus for both Mac and iOS, but it turns out that because I lean so heavily on using Siri to add items, I tend not to open OmniFocus while I'm on the go. When I come home and get to work on my Mac, I notice that OmniFocus doens't contain any of my recently added items, so I have to go through the cumbersome steps of opening my iPhone and launching OmniFocus <em>just to get this theoretically time-saving trick to work right.</em>
+
+RemnindersImport solves this problem by adding behavior to my Mac that strongly emulates the behavior built in to OmniFocus for iOS. When launched, the tool will scan for non-location-based reminders, add them to OmniFocus (with start and due dates in-tact!), and then remove them from Apple's reminders list.
+
+Usage
+=====
+
+<strong>Warning:</strong> RemindersImport is designed to scan your Mac OS X Reminders <em>and remove them</em> from the default location in your Reminders list so that they may be added instead to OmniFocus. You should understand very well that this is what you want to do before building and running the tool.
+
+Let's say you have 5 Reminders that you added via Siri on your phone. In the background, thanks to Apple's aggressive syncing, these have been migrated over to your Mac and are now visible in Reminders.app. To migrate these from Reminders to OmniFocus, just run the tool once:
+
+<pre>
+% ./RemindersImport
+</pre>
+
+If you've opted to use a different reminders list for OmniFocus, you can specify the name on the command line to import from that list instead:
+
+<pre>
+% ./RemindersImport "Junk to Do"
+</pre>
+
+Automatic Execution
+====================
+
+Running the tool by hand is about as annoying as having to remember to open up the iPhone and launch OmniFocus, so ideally you'll want to set this thing up to run on its own automatically. I haven't yet settled on the ideal approach for this, but a crude way of setting it up would be to just use Mac OS X's built-in cron scheduling service to run the tool very often, say every minute:
+
+<pre>
+*/1 * * * * /Users/daniel/bin/RemindersImport
+</pre>
+
+Something I'd like to look into is whether it would make sense to set this tool up as lightweight daemon that just stays running all the time, waiting for Reminders database changes to happen, and then snagging stuff. For now, the crontab based trick is doing the job well enough for my needs.
---
+++ main.m
@@ -0,0 +1,187 @@
+//
+//  main.m
+//  RemindersImport
+//
+//  Created by Daniel Jalkut on 1/30/13.
+//  Copyright (c) 2013 Bitsplitting. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <EventKit/EventKit.h>
+
+NSArray* importableReminderItems(EKEventStore* theEventStore)
+{
+	NSMutableArray* qualifiedItems = [NSMutableArray array];
+	NSString* remindersListName = @"Reminders";
+	if ([[[NSProcessInfo processInfo] arguments] count] < 2)
+	{
+		NSLog(@"No Reminders list name specified, defaulting to \"%@\"", remindersListName);
+	}
+	else
+	{
+		remindersListName = [[[NSProcessInfo processInfo] arguments] objectAtIndex:1];
+	}
+
+	// Get all events from the "Reminders" list
+	__block BOOL done = NO;
+
+	// Get all the dedicated Reminders calendars
+	NSArray* remindersCalendars = [theEventStore calendarsForEntityType:EKEntityTypeReminder];
+
+	NSPredicate *remindersPredicate = [theEventStore predicateForIncompleteRemindersWithDueDateStarting:nil ending:nil calendars:remindersCalendars];
+	[theEventStore fetchRemindersMatchingPredicate:remindersPredicate completion:^(NSArray *reminders)
+	{
+		// Since fetchRemindresMatchingPredicate doesn't actually work with general-purpose NSPredicate,
+		// we have to use one of the coarse-grained EKEventStore-vended predicates, and then further filter on
+		// our own. This predicate attempts to match The Omni Group's own filtering approach: if the reminder
+		// is in the list and is not a location-based reminder, just snag it and delete it, otherwise,
+		// leave it alone.
+		//
+		// NOTE: I'm being lazy for starters and not figuring out how to specify that the matched reminder
+		// should have NO alarms that feature a non-nil structuredLocation (i.e. the alarm is not to remind when
+		// arriving at a speicfic location). I just filter those out in the for loop below.
+		NSPredicate* fineGrainPredicate = [NSPredicate predicateWithFormat:@"calendar.title = %@", remindersListName];
+		NSArray* targetReminderItems = [reminders filteredArrayUsingPredicate:fineGrainPredicate];
+
+		for (EKReminder* thisReminder in targetReminderItems)
+		{
+			BOOL hasLocationBasedAlarm = NO;
+			
+			for (EKAlarm* thisAlarm in [thisReminder alarms])
+			{
+				if ([thisAlarm structuredLocation] != nil)
+				{
+					hasLocationBasedAlarm = YES;
+					break;
+				}
+			}
+
+			if (hasLocationBasedAlarm == NO)
+			{
+				[qualifiedItems addObject:thisReminder];
+			}
+		}
+		done = YES;
+	}];
+
+	while (done == NO)
+	{
+		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.1]];
+	}
+
+	return qualifiedItems;
+}
+
+NSString* omniFocusAddTaskScriptTemplate()
+{
+	return @"\
+-- Work around a problem where if OmniFocus is not already running,\n\
+-- it might hang when trying to handle our request just as it's launching\n\
+set isRunning to true\n\
+tell application \"System Events\"\n\
+	try\n\
+		application process \"OmniFocus\"\n\
+	on error\n\
+		set isRunning to false\n\
+	end try\n\
+end tell\n\
+\n\
+-- Wait a good long 10 seconds to make sure OmniFocus has time to launch\n\
+if (isRunning is false) then\n\
+	tell application \"OmniFocus\" to launch\n\
+	delay 10.0\n\
+end if\n\
+\
+tell application \"OmniFocus\"\n\
+	tell default document\n\
+		make new inbox task with properties {name:\"%@\", start date:date \"%@\", due date:date \"%@\"}\n\
+	end tell\n\
+end tell";
+}
+
+BOOL makeOmniFocusInboxTaskFromReminderInfo(NSString* newTaskTitle, NSString* startDateString, NSString* dueDateString)
+{
+	BOOL didSucceed = NO;
+	NSString* newTaskAppleScript = [NSString stringWithFormat:omniFocusAddTaskScriptTemplate(), newTaskTitle, startDateString, dueDateString];
+	NSAppleScript* addTaskScript = [[[NSAppleScript alloc] initWithSource:newTaskAppleScript] autorelease];
+	NSDictionary* anyError = nil;
+	(void) [addTaskScript executeAndReturnError:&anyError];
+
+	if (anyError != nil)
+	{
+		NSLog(@"Failed to add task, got scripting error: %@", anyError);
+		didSucceed = NO;
+	}
+	else
+	{
+		NSLog(@"Added task \"%@\" to OmniFocus.", newTaskTitle);
+		didSucceed = YES;
+	}
+
+	return didSucceed;
+}
+
+NSCalendar* gregorianCalendar()
+{
+	return [[[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar] autorelease];;
+}
+
+NSDateFormatter* gregorianFormatterWithFormat(NSString* formatString)
+{
+	NSDateFormatter* gregorianFormatter = [[[NSDateFormatter alloc] init] autorelease];
+	[gregorianFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+	[gregorianFormatter setDateFormat:formatString];
+	[gregorianFormatter setCalendar:gregorianCalendar()];
+	return gregorianFormatter;
+}
+
+NSString* scriptableDateStringFromComponents(NSDateComponents* inComponents)
+{
+	// This is in Unicode format as described here: http://userguide.icu-project.org/formatparse/datetime,
+	// and designed to match the example like "Thursday, January 31, 2013 4:32:40 PM". I observed this
+	// format as the default format returned by AppleScript on my US-based English Mac. I can't vouch
+	// for how well this will work in other locales, but let me know if you try it!
+	NSString* dateFormatString = @"EEEE, LLLL d y hh:mm:ss a";
+	NSDate *targetDate = [gregorianCalendar() dateFromComponents:inComponents];
+	NSDateFormatter* dateFormatter = gregorianFormatterWithFormat(dateFormatString);
+	return [dateFormatter stringFromDate:targetDate];
+}
+
+int main(int argc, const char * argv[])
+{
+	@autoreleasepool
+	{
+		EKEventStore* eventStore = [[EKEventStore alloc] initWithAccessToEntityTypes:EKEntityMaskReminder];
+
+		for (EKReminder* thisReminder in importableReminderItems(eventStore))
+		{
+			// Extract the information we need to ask OmniFocus to add the item
+			NSString* newTaskName = [thisReminder title];
+
+			NSString* startDateString = scriptableDateStringFromComponents([thisReminder startDateComponents]);
+			NSString* dueDateString = scriptableDateStringFromComponents([thisReminder dueDateComponents]);
+
+			if (makeOmniFocusInboxTaskFromReminderInfo(newTaskName, startDateString, dueDateString) == YES)
+			{
+				NSLog(@"Removing task %@ from event store", newTaskName);
+				NSError* removeError = nil;
+				if ([eventStore removeReminder:thisReminder commit:NO error:&removeError] == NO)
+				{
+					NSLog(@"Failed to remove reminder %@ from event store. Error: %@", newTaskName, [removeError localizedDescription]);
+				}
+			}
+		}
+
+		// We queued up any removes by specifying commit:NO, so let's commit now to finish the job
+		NSError* commitError = nil;		
+		if ([eventStore commit:&commitError] == NO)
+		{
+			NSLog(@"Failed to commit reminder removes. Error: %@", [commitError localizedDescription]);
+		}
+
+		// Clean up because it just feels right.
+		[eventStore release];
+	}
+    return 0;
+}
+
